9c947ea19486143727ebf03aa9fa9ff5ea70532a
Chris Bowyer <100442597+Devcab2@users.noreply.github.com>
2022-06-29 11:39:47 -0600

    exported connect function using object destructuring

--- play.js
+++ play.js
@@ -1,23 +1,2 @@
-const net = require("net");
-
-// esablish a conection to game server
-
-const connect = function() {
-  const conn = net.createConnection({
-
-    host: '192.168.1.81',
-    port: 50541
-
-  });
-  // interepret data as incoming text
-  conn.setEncoding("utf8");
-  
-  // log a message to the client after recieiving data
-  conn.on("data", () => {
-    console.log("you ded cuz u idled");
-  });
-  return conn;
-};
-
-console.log("Connecting ...");
+const {connect} = require('client.js');
 connect();
