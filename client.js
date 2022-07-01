bc41c8c8c0b1177556777dce6fdeb7195471bd84
Chris Bowyer <100442597+Devcab2@users.noreply.github.com>
2022-07-01 09:58:19 -0600

    sending data to server and printing it to the client

--- client.js
+++ client.js
@@ -1,19 +1,34 @@
 const net = require("net");
+const { setInterval } = require("timers/promises");
+
 
 const conn = net.createConnection({
-
   host: '192.168.1.81',
   port: 50541
-
 });
- 
+
 conn.setEncoding("utf8");
-  
+
 conn.on("data", (messageToClient) => {
   console.log(`Name:${messageToClient}`);
+});
+
+/*
+conn.on("data", () => {
+  setInterval(() => {
+    console.log("Move: up");
+  }, 50);
+  setTimeout(() => {
+    console.log("Move: left");
+  }, 500);
+});
+*/
+
+conn.on("data", () => {
   console.log("you ded cuz u idled");
 });
 
+
 console.log("Connecting ...");
 
-conn.write("Hello server: I'am client");
+console.log("Hello server: I'am client");
--- play.js
+++ play.js
@@ -2,24 +2,23 @@ const net = require('net');
 const port = 50541;
 const server = net.createServer();
 
+
 server.on('connection', (client) => {
   console.log('Successfully connected to game server');
 
   client.write('Name:CAB');
-
-  //connection.on('data', (messageFromClient) => {
-  //  console.log('The client says:', messageFromClient);
- 
 });
 
 
-
+//connection.on('data', (messageFromClient) => {
+//  console.log('The client says:', messageFromClient);
+ 
 //connection.setEncoding('utf-8',);
 //});
 
+
 server.listen(port, () => {
   console.log(`The server is listening on port:${port}`);
-
 });
 
 
